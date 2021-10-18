7bdd47cb0a5f406cad1df5774205a280fbad0388
lukions <92707917+lukions@users.noreply.github.com>
2021-10-18 17:03:56 -0500

    Add files via upload

---
+++ src/juegos.js
@@ -0,0 +1,21 @@
+const juegos = (prefix, pushname) => {
+    return `
+*Comandos De Juegos 👾*
+ 
+${prefix}rankgay
+Conoce a el top de los mas gays del grupo
+
+${prefix}gay
+Conoce que tan gay eres 😎
+
+${prefix}cuties
+Prueba tu suerte en el medidor de fan de cuties 😱
+
+_*En futuras versiones colocare mas juegos :)*_
+     
+_*by shanduy*_
+`
+
+}
+
+exports.juegos = juegos
---
+++ src/version.js
@@ -0,0 +1,33 @@
+const version = (prefix, pushname) => {
+    return `
+*ѕнαηвσт ву ѕнαη∂υу*
+
+*Actualizado:* 18 de Septiembre del 2021
+*Versión actual:* 4.0
+*Ofrecida por:* shanduy™
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+
+
+*COMO ACTUALIZAR EL BOT*
+
+https://cutt.ly/dWp5uqE
+
+
+*SIGUEME EN INSTAGRAM 🥸🧉*
+
+https://www.instagram.com/thepavos
+
+
+_*by shanduy*_
+`
+
+}
+
+exports.version = version
